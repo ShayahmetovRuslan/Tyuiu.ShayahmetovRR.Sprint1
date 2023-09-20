cbf5cb10a6e23594198a5447763df0a9d87b0efd
Tyuiu.ShayahmetovRR@outlook.com <ruslann12@list.ru>
2023-09-20 23:16:30 +0500

    Task1

---
+++ Tyuiu.ShayahmetovRR.Sprint1.Task1.V25.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.ShayahmetovRR.Sprint1.Task1.V25.Lib
+{
+    public class DataService : ISprint1Task1V25
+    {
+        public double Calculate(double x, double y)
+        {
+            return (x * y) / (1 + x);
+        }
+    }
+}
---
+++ Tyuiu.ShayahmetovRR.Sprint1.Task1.V25/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.ShayahmetovRR.Sprint1.Task1.V25.Lib;
+
+namespace Tyuiu.ShayahmetovRR.Sprint1.Task1.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Шаяхметов Р. Р.  | АСОиУБ-23-1";
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* Спринт #1                                                            *");
+            Console.WriteLine("* Тема: Организация ввода\вывода в консольных приложениях              *");
+            Console.WriteLine("* Задание #1                                                           *");
+            Console.WriteLine("* Вариант #25                                                          *");
+            Console.WriteLine("* Выполнил: Шаяхметов Руслан Раилевич  | АСОиУБ-23-1                   *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                             *");
+            Console.WriteLine("* Написать консольную программу на C#, которая вычисляет выражение     *");
+            Console.WriteLine("*                                     по формуле  (x*y)/(1+x)          *");
+            Console.WriteLine("*                                                                      *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ: (5*2)/(1+5)                                         *");
+            Console.WriteLine("************************************************************************");
+            Console.WriteLine(" РЕЗУЛЬТАТ:                                                            *");
+            Console.WriteLine("************************************************************************");
+
+            Console.WriteLine(ds.Calculate(5.0, 2.0));
+            Console.ReadLine();
+        }
+    }
+}
